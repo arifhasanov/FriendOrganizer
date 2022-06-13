263bdb5c2e94cc4c77d42c678af72681f32b3379
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2022-06-13 11:16:29 +0200

    tt

---
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace FriendOrganizer.UI.Startup;
+public class Bootstrapper
+{
+    public Autofac.IContainer Bootstrap()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<MainWindow>().AsSelf();
+        builder.RegisterType<MainViewModel>().AsSelf();
+        builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+
+        return builder.Build();
+    }
+}
